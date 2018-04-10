b90dbb3c27879143f71d8f33d2f52274771afce1
danieltoo <torresestradadaniel@gmail.com>
2018-04-09 23:14:07 -0500

    Cambie mysql

--- config/config.js
+++ config/config.js
@@ -1,18 +1,18 @@
 //Local database
-exports.mysql = {
+/*exports.mysql = {
   host : 'localhost',
   db : 'prueba',
   user : 'root',
   password : ''
-}
+}*/
 exports.keyrock = "207.249.127.96:8001"
 //Remote database
-/*exports.mysql = {
+exports.mysql = {
   host : '207.249.127.16',
   db : 'smarsdkcenidet',
   user : 'smartsdkcenidet',
   password : 'hdcenidet2018*'
-}*/
+}
 exports.context = {
   //host : 'http://207.249.127.218',
   host:'http://130.206.113.226',
