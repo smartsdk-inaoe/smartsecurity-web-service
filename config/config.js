fe0f6cfa33f8a3837f7b2f7b32456a5493e6e297
Daniel Torres <torresestradadaniel@gmail.com>
2018-06-19 11:08:51 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -9,12 +9,14 @@
 exports.keyrock = "207.249.127.96:8001"
 //Remote database
 exports.mysql = {
-  host : '200.23.5.144',
-  //host : '207.249.127.16',
-  db : 'smartsdksecurity',
-  //user : 'smartsdkcenidet',
-  user : 'cenidet',
-  password : 'Cenidet2017'
+  host : '35.196.174.137',
+  db : 'smarsdkcenidet',
+  user : 'root,
+  password : 'hdcenidet2018*'
+  //host : '200.23.5.144',
+  //db : 'smartsdksecurity',
+  //user : 'cenidet',
+  //password : 'Cenidet2017'
 }
 exports.context = {
   //host : 'http://207.249.127.218',
