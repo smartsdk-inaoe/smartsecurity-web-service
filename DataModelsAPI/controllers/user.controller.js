28b425580251ec4b75aa0f7a7d37e1952bcd46a1
Daniel Torres <torresestradadaniel@gmail.com>
2018-06-19 11:41:32 -0500

    Update user.controller.js

--- DataModelsAPI/controllers/user.controller.js
+++ DataModelsAPI/controllers/user.controller.js
@@ -18,7 +18,8 @@ var headers = {
 	"accept-language": "en-US,en;q=0.8",
 	"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36",
 	"content-type": "application/json",
-	"X-Auth-token": "ADMIN"
+	"X-Auth-token": "ADMIN",
+	"authorization" : "Basic aWRtOmlkbQ=="
 }
 
 exports.add = function (req, res){
