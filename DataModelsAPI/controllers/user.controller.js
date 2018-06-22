0fd4736aa5290edea16eef43c21581fb48268663
danieltoo <torresestradadaniel@gmail.com>
2018-06-22 13:56:35 -0500

    Cambie ruta de keystone

--- DataModelsAPI/controllers/user.controller.js
+++ DataModelsAPI/controllers/user.controller.js
@@ -19,8 +19,7 @@ var headers = {
 	"accept-language": "en-US,en;q=0.8",
 	"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36",
 	"content-type": "application/json",
-	"X-Auth-token": "ADMIN",
-	"authorization" : "Basic aWRtOmlkbQ=="
+	"X-Auth-token": "ADMIN"
 }
 
 exports.add = function (req, res){
