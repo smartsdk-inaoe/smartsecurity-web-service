a3238c38be1e1950664d5989785a53cf8394383d
danieltoo <torresestradadaniel@gmail.com>
2018-08-16 12:49:03 -0500

    i comment the login temporaly

--- DataModelsAPI/controllers/user.controller.js
+++ DataModelsAPI/controllers/user.controller.js
@@ -175,7 +175,7 @@ exports.keyLogin = (req, res) => {
 						})
 						let token = response.headers._headers['x-subject-token'][0];
 						console.log(token)
-						res.status(200).json({token : token, user})
+						res.status(200).json({token : "token", user})
 					})
 					.catch((err) => {
 						res.status(404).json(err)
