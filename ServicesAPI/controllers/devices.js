6366902f5b3133cfabe7d7c36a89ecf5209cc8a3
danieltoo <torresestradadaniel@gmail.com>
2018-08-17 17:27:12 -0500

    i added local time zone Mexico

--- ServicesAPI/controllers/devices.js
+++ ServicesAPI/controllers/devices.js
@@ -9,7 +9,8 @@ exports.getZone = async function (req,res) {
 	await Zone.findOne({where : { 'idZone': req.params.idZone }})
     .then( async (zone) => {
 	  	if (zone != null){
-			var dt = DateTime.utc();
+			//var dt = DateTime.utc();
+			var dt = DateTime.local().setZone('America/Mexico_City');
 			let fifteenAgo = dt.minus({ minutes: 15 });
 			let query = ngsi.createQuery(Object.assign({
 				id: "Device_Smartphone_.*",
@@ -45,7 +46,8 @@ exports.getZoneByOwner = async function (req,res) {
 			await user.findOne({ where : queries})
 			.then ( async(user) =>{
 				if (user!= null){
-					var dt = DateTime.utc();
+					//var dt = DateTime.utc();
+					var dt = DateTime.local().setZone('America/Mexico_City');
 					let fifteenAgo = dt.minus({ minutes: 15 });
 					let query = ngsi.createQuery({
 						id: "Device_Smartphone_.*",
