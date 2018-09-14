9eacfca58b84b77273bfd40286614ffb92b39246
Daniel Torres <torresestradadaniel@gmail.com>
2018-09-14 09:54:47 -0500

    Update alertsZone.js

--- ServicesAPI/controllers/alertsZone.js
+++ ServicesAPI/controllers/alertsZone.js
@@ -104,6 +104,8 @@ exports.getCurrent = async function (req,res) {
 
 				if(count > 20)
 					jsonQuery2["offset"] = count - 10;
+		    		console.log(count)
+		    		console.log(jsonQuery2["offset"])
 
 				let query = ngsi.createQuery(jsonQuery2);
 
